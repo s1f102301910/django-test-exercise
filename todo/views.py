7c3589bd090d82f880d507464395bfc7f3820c61
1F10230175 <s1F102301752@iniad.org>
2024-07-18 11:45:34 +0900

    Edit機能の間違い修正とCSSファイルの準備

--- todo/views.py
+++ todo/views.py
@@ -40,7 +40,7 @@ def update(request, task_id):
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     if request.method == "POST":
-        task.title = request.POST['titile']
+        task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
